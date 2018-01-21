bd2dbd6c9ee61d40a580d98e27df5134077bbcee
cperez2187 <cperez2187@gmail.com>
2018-01-21 08:02:20 -0700

    Added music button and shuffle music on each load.

--- music/music.js
+++ music/music.js
@@ -8,19 +8,55 @@
     './music/bensound-funkyelement.mp3',
   ];
 
+
   // Create Howl instance
   var sound = new Howl({
-    src: music,
-    // autoplay: true,
+    src: shuffle(music),
     loop: true,
     volume: 0.5,
     preload: true,
     html5: true
   });
 
+  var play = false;
+
   sound.on('load', function() {
-    sound.play();
+    $('.music-button').on('click', function() {
+      play = !play;
+      console.log(play);
+      if (play) {
+        // sound.play() && sound.mute(false);
+        sound.playing() ? sound.mute(false) : sound.play();
+        $(this).toggleClass('glyphicon-play glyphicon-volume-off');
+        $(this).attr('aria-label', 'Mute Music');
+
+      } else {
+        sound.mute(true);
+        $(this).toggleClass('glyphicon-play glyphicon-volume-off');
+        $(this).attr('aria-label', 'Play Music');
+      }
+    });
+
   });
 
 
 })()
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  console.log(array);
+  return array;
+}
