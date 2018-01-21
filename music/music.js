8571bde511a3ac6970fc505eb24dc1923a986125
cperez2187 <cperez2187@gmail.com>
2018-01-20 18:40:45 -0700

    update opening track.

--- music/music.js
+++ music/music.js
@@ -1,11 +1,11 @@
 (function() {
   //Put music files in array
   var music = [
+    './music/bensound-love.mp3',
+    './music/bensound-tenderness.mp3',
+    './music/bensound-straight.mp3',
     './music/bensound-acousticbreeze.mp3',
     './music/bensound-funkyelement.mp3',
-    './music/bensound-love.mp3',
-    './music/bensound-straight.mp3',
-    './music/bensound-tenderness.mp3',
   ];
 
   // Create Howl instance
