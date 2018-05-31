1ca242de92ad5f686540db81969f367d8e788b31
cperez2187 <cperez2187@gmail.com>
2018-05-30 22:12:58 -0700

    Material UI added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/button';
+
+const App = () => {
+  return (
+    <Button variant="raised" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+export default App;
