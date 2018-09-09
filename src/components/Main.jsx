1c969b107a790743e7f71ce762f0eeeb7fce33f6
cperez2187 <cperez2187@gmail.com>
2018-09-08 21:54:20 -0700

    Added floating action add button for new posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
 import PermanentDrawer from './PermanentDrawer';
 import CreateBlog from './CreateBlog';
 
@@ -12,67 +14,75 @@ const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   appFrame: {
     zIndex: 1,
     overflow: 'hidden',
     position: 'relative',
     display: 'flex',
-    width: '100%',
+    width: '100%'
   },
   appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
+    width: `calc(100% - ${drawerWidth}px)`
   },
   'appBar-left': {
-    marginLeft: drawerWidth,
+    marginLeft: drawerWidth
   },
   'appBar-right': {
-    marginRight: drawerWidth,
+    marginRight: drawerWidth
   },
   drawerPaper: {
     position: 'relative',
-    width: drawerWidth,
+    width: drawerWidth
   },
   toolbar: theme.mixins.toolbar,
+  toolbarFlex: {
+    display: 'flex',
+    justifyContent: 'space-between' 
+  },
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 3
   },
+  button: {
+    margin: theme.spacing.unit
+  }
 });
 
 class Main extends Component {
   state = {
-    anchor: 'left',
+    anchor: 'left'
   };
 
   render() {
-    const { classes } = this.props;
-    const { anchor } = this.state;
+    const {classes} = this.props;
+    const {anchor} = this.state;
 
     return (
       <div className={classes.root}>
         <div className={classes.appFrame}>
           <AppBar
             position="absolute"
-            className={classNames(classes.appBar, classes[`appBar-${anchor}`])}
-          >
-            <Toolbar>
+            className={classNames(classes.appBar, classes[`appBar-${anchor}`])}>
+            <Toolbar className={classes.toolbarFlex}>
               <Typography variant="title" color="inherit" noWrap>
                 Anthony's Blog Posts
               </Typography>
+              <Button
+                variant="fab"
+                color="secondary"
+                aria-label="Add"
+                className={classes.button}>
+                <AddIcon/>
+              </Button>
             </Toolbar>
           </AppBar>
-          <PermanentDrawer
-            variant="permanent"
-            classes={classes}
-            anchor={anchor} 
-          />
-          {/* Main content */}
+          <PermanentDrawer variant="permanent" classes={classes} anchor={anchor}/> {/* Main content */}
           <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <CreateBlog />
+            <div className={classes.toolbar}/>
+            <CreateBlog/>
           </main>
         </div>
       </div>
@@ -81,7 +91,7 @@ class Main extends Component {
 }
 
 Main.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Main);
